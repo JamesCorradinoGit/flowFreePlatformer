acb0f935f4de0812bc24b9d3547a97a694e3278a
JamesCorradinoGit <corradinojames34@gmail.com>
2026-06-13 10:50:12 -0500

    levels

--- Scripts/levelScript.gd
+++ Scripts/levelScript.gd
@@ -5,6 +5,7 @@ class_name level
 @export var goalPortal:levelPortal
 @export var resetTileset:TileMapLayer
 @export var hasGrids:bool = true
+@export var gridParent:Node
 
 var player:PackedScene = load("uid://cvslsain1kjbi")
 var numGrids:int = 0
@@ -21,27 +22,27 @@ func _ready() -> void:
 	var tempPlayer:CharacterBody2D = player.instantiate().duplicate()
 	tempPlayer.global_position = playerSpawn.global_position
 	add_child(tempPlayer)
-	if self.hasGrids and self.find_child("grids") != null:
-		for grid:gridObject in self.find_child("grids").get_children():
+	if self.hasGrids and self.gridParent != null:
+		for grid:gridObject in self.gridParent.get_children():
 			numGrids += 1
 			grid.gridCompletedSig.connect(gridComplete)
 			grid.gridCompleteBreakSig.connect(gridBreak)
+	else:
+		gridsComplete = true
 
 func _process(_delta: float) -> void:
 	if gridsComplete and portalInteracted:
+		print("all done")
 		completeLevel.emit()
 		set_process(false)
 
 func gridComplete():
 	instGridsCompleted += 1
 	if self.hasGrids and instGridsCompleted == numGrids:
-		print("grids done")
 		gridsComplete = true
 func gridBreak():
 	instGridsCompleted -= 1
-	print("grid break")
 	gridsComplete = false
 
 func onPortalReached():
-	print("done")
 	portalInteracted = true
