07eca57fe6f9c1b102bb2f4c22b76a9159d408f2
JamesCorradinoGit <corradinojames34@gmail.com>
2026-06-22 12:29:11 -0500

    ui

---
+++ Scripts/ui/sceneSwitcherButton.gd
@@ -0,0 +1,28 @@
+extends Button
+
+@export var sceneToSwitch: PackedScene
+@export_subgroup("Tweens")
+@export var doTween: bool = false
+@export var tweenOffset: Vector2
+@export var tweenTime: float = 0.25
+
+var combinedTweenPos:Vector2
+var originPos:Vector2
+
+#CONNECT SIGNALS
+
+func _ready() -> void:
+	if self.doTween:
+		originPos = global_position
+		combinedTweenPos = global_position + tweenOffset
+
+func _on_pressed() -> void:
+	GlobalSceneLoader.loadScene(str(sceneToSwitch.resource_path))
+
+func _on_mouse_entered() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", combinedTweenPos, tweenTime)
+
+func _on_mouse_exited() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", originPos, tweenTime)
